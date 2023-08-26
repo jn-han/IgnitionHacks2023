3cd4c17e6ef236345581a8075998d523728975b8
Josh Nguyen <joshuahnguyen@gmail.com>
2023-08-25 21:57:31 -0700

    Initial Commit

---
+++ IgnitionHacks2023/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  IgnitionHacks2023
+//
+//  Created by Joshua Nguyen on 2023-08-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        //Mark - Body
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("This is a test")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IgnitionHacks2023/IgnitionHacks2023App.swift
@@ -0,0 +1,17 @@
+//
+//  IgnitionHacks2023App.swift
+//  IgnitionHacks2023
+//
+//  Created by Joshua Nguyen on 2023-08-25.
+//
+
+import SwiftUI
+
+@main
+struct IgnitionHacks2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
