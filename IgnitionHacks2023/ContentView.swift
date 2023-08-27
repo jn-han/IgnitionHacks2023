faba57f75ab6b40dd7c858ec945ab7f364ac3e26
Josh Nguyen <joshuahnguyen@gmail.com>
2023-08-27 01:38:42 -0700

    Restructure on UI

--- IgnitionHacks2023/ContentView.swift
+++ IgnitionHacks2023/ContentView.swift
@@ -9,68 +9,32 @@ import SwiftUI
 import OpenAISwift
 
 final class ViewModel: ObservableObject {
-    init() {}
-    
-    private var client: OpenAISwift?
-    
-    func setup() {
-        client = OpenAISwift(config: .makeDefaultOpenAI(apiKey: "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"))
-    }
-    
-    func send(text: String, completion: @escaping(String) -> Void) {
-        client?.sendCompletion(with: text,
-                               maxTokens: 500,
-                               completionHandler: { result in
-            switch result {
-            case .success(let model):
-                print(model.choices?.first?.text ?? "")
-                let output = model.choices?.first?.text ?? ""
-                completion(output)
-            case .failure(let failure):
-                print(failure.localizedDescription)
-            }
-        })
-    }
+
 }
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ViewModel()
-    @State var text = ""
-    @State var models = [String]()
-    
+    @State var chatMessages: [ChatMessage] = ChatMessage.sampleMessages
     var body: some View {
-        VStack (alignment: .leading) {
-            ForEach(models, id: \.self) { string in
-                Text(string)
-            }
-            
-            
-            Spacer()
-
-            HStack {
-                TextField("Type here...", text: $text)
-                Button("Send") {
-                    send()
+        VStack{
+            ScrollView {
+                LazyVStack {
+                    ForEach(chatMessages, id: \.id) { message in
+                        messageView(message: message)
+                    }
                 }
             }
         }
-        .onAppear() {
-            viewModel.setup()
-        }
         .padding()
     }
-
-    func send() {
-        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
-            return
-        }
-        models.append("Me: \(text)")
-        viewModel.send(text: text) { response in
-            DispatchQueue.main.async {
-
-                self.models.append("Bot: " + response)
-                self.text = ""
-            }
+    
+    func messageView(message: ChatMessage) -> some View {
+        HStack{
+            if message.sender == .me {Spacer()}
+            Text(message.content)
+                .foregroundColor(message.sender == .me ? .white : .black)
+                .padding()
+                .background(message.sender == .me ? .blue : .gray.opacity(0.1))
+            if message.sender == .gpt {Spacer()}
         }
     }
 }
@@ -80,4 +44,37 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct ChatMessage {
+    let id: String
+    let content: String
+    let dateCreated: Date
+    let sender: MessageSender
+}
+
+enum MessageSender {
+    case me
+    case gpt
+}
+
+extension ChatMessage {
+    static let sampleMessages = [
+        ChatMessage(id: UUID().uuidString,
+                    content: "Sample Message from me",
+                    dateCreated: Date(),
+                    sender: .me),
+        ChatMessage(id: UUID().uuidString,
+                    content: "Sample Message from me",
+                    dateCreated: Date(),
+                    sender: .gpt),
+        ChatMessage(id: UUID().uuidString,
+                    content: "Sample Message from me",
+                    dateCreated: Date(),
+                    sender: .me),
+        ChatMessage(id: UUID().uuidString,
+                    content: "Sample Message from me",
+                    dateCreated: Date(),
+                    sender: .gpt),
+        ]
 }
