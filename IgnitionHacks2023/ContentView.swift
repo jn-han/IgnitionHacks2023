7f87640c75753eff214334eb6c8a3f129e58e54e
Josh Nguyen <joshuahnguyen@gmail.com>
2023-08-27 01:22:17 -0700

    Added OpenAISwift Package + light chat setup.

--- IgnitionHacks2023/ContentView.swift
+++ IgnitionHacks2023/ContentView.swift
@@ -6,22 +6,75 @@
 //
 
 import SwiftUI
+import OpenAISwift
+
+final class ViewModel: ObservableObject {
+    init() {}
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(config: .makeDefaultOpenAI(apiKey: "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"))
+    }
+    
+    func send(text: String, completion: @escaping(String) -> Void) {
+        client?.sendCompletion(with: text,
+                               maxTokens: 500,
+                               completionHandler: { result in
+            switch result {
+            case .success(let model):
+                print(model.choices?.first?.text ?? "")
+                let output = model.choices?.first?.text ?? ""
+                completion(output)
+            case .failure(let failure):
+                print(failure.localizedDescription)
+            }
+        })
+    }
+}
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
     var body: some View {
-        
-        
-        //Mark - Body
-        //hello
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("This is a piece of text")
+        VStack (alignment: .leading) {
+            ForEach(models, id: \.self) { string in
+                Text(string)
+            }
+            
+            
+            Spacer()
+
+            HStack {
+                TextField("Type here...", text: $text)
+                Button("Send") {
+                    send()
+                }
+            }
+        }
+        .onAppear() {
+            viewModel.setup()
         }
         .padding()
     }
+
+    func send() {
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        models.append("Me: \(text)")
+        viewModel.send(text: text) { response in
+            DispatchQueue.main.async {
+
+                self.models.append("Bot: " + response)
+                self.text = ""
+            }
+        }
+    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
